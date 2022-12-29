7b471b298b640782e9a724713ec0fb1adb9788e9
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2022-12-29 21:36:43 +0530

    Linear Search Implemented

---
+++ DSA/List/lineraSearch.py
@@ -0,0 +1,18 @@
+# Find element in the  list (Linear Search)
+number = []
+size = int(input("Enter number of elements :"))
+
+# To get all the elements
+for i in range(size):
+    number.append(int(input(" Enter the number : ")))
+    
+data = int(input("Enter value to search : "))
+
+for i in range(len(number)):
+    if data == number[i]:
+        print(f"Elememt is present at the {i+ 1}th index")
+        # Exit for first occurance
+        exit()
+
+# If data is not present in the list
+print(" Element is not present in the list") 
