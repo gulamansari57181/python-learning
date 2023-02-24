ecb4db49a3057d6a1282228b9cf755b82666c81e
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2023-02-24 11:06:51 +0530

    make dynamic route for each month

--- DJango Projects/monthly_challenges/challenge/urls.py
+++ DJango Projects/monthly_challenges/challenge/urls.py
@@ -6,5 +6,9 @@ from . import views
 urlpatterns = [
     # part() accepts two parameters 1)String that is url we want to support 
     # 2) corresponding views for that we have to import views.py file
-    path("january",views.index)
+    # path("january",views.index),
+    # path("febuary",views.febuary),
+    # To handle any string url passed. we will use place holder syntax
+    
+    path("<month>",views.monthly_challenge)
 ]
--- DJango Projects/monthly_challenges/challenge/views.py
+++ DJango Projects/monthly_challenges/challenge/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+# To create month: challenge map
+
+challenge_text={
+    "january":"Learn django everyday for 30 minutes.",
+    "febuary":"Do running for 1 hour.",
+    "march":"Learn DSA fundamentals.",
+    "april":"Learn Advance concepts of DSA.",
+    "may":"Solve  2 leetcode problems daily.",
+    "june":"Learn swimming.",
+    "july":"Learn React.js for frontend development.",
+    "august":"Make web dev projects.",
+    "september":"Go for holiday trip.",
+    "october":"Eat vegetable only for a month.",
+    "november":"Visit NGo and help the needy people.",
+    "december":"Party day in and day out !!!"
+    
+}
+
+# Create your views here.
 def index(request):
-    return HttpResponse("Hello How you doing ??????")
+    return HttpResponse("<h1>Hello How you doing ??????</h1>")
+
+# Create a view for febuary
+def monthly_challenge(request, month):
+    
+    challenge=challenge_text.get(month)
+    return HttpResponse(f"<h1>Challenge : {challenge} </h1>") 
