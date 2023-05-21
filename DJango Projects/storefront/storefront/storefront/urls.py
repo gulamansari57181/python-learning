706b8f9cb2b86019956ef166c3a449bf0b45d4ce
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2023-05-21 20:28:32 +0530

    New url and views added

--- DJango Projects/storefront/storefront/storefront/urls.py
+++ DJango Projects/storefront/storefront/storefront/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import playground
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    playground('home/',playground.urls),
 ]
