4737786c3a616aeb4b49d4aaaa5d9095b1d30c81
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2023-05-22 21:42:43 +0530

    build first route

--- DJango Projects/storefront/storefront/storefront/urls.py
+++ DJango Projects/storefront/storefront/storefront/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import playground
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    playground('home/',playground.urls),
+    path('home/',include("playground.urls")),
 ]
