b31ce29c84191d0e7e9b22827661156097c3f09b
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2024-04-16 23:50:04 +0530

    Palindrome using recursion

---
+++ DSA/Recursion/palindrome.py
@@ -0,0 +1,18 @@
+str=input("Enter a string :")
+
+def palindrome(str,left,right):
+    
+    if left>=right:
+        return True
+    
+    if str[left]==str[right]:
+        return palindrome(str,left+1,right-1)
+    
+    return False
+
+
+
+if palindrome(str,0,len(str)-1):
+    print(f"{str} is palindrome string!!")
+else:
+    print(f"{str} is not palindrome string!!")
